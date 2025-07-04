33d778429e7da67706f7aeddab0ab1f0fcb94593
Atharv Patil <atharvpatil269@gmail.com>
2025-07-04 11:30:17 +0530

    solved Attributes Errors

--- src/app/bots/perplexity/main.py
+++ src/app/bots/perplexity/main.py
@@ -333,10 +333,10 @@ if __name__ == "__main__":
     # ---------- DrissionPage headless config ----------
     co = ChromiumOptions()
     co.set_browser_path(os.environ["DP_BROWSER_PATH"])
-    co.set_argument("--headless=new")          # modern headless mode
-    co.set_argument("--no-sandbox")            # safe in containers/VMs
-    co.set_argument("--disable-dev-shm-usage") # avoid /dev/shm limits
-    co.set_argument("--remote-debugging-port=0")  # pick free port
+    co.set_argument("--headless=new")           # modern headless mode
+    co.set_argument("--no-sandbox")             # needed on many servers
+    co.set_argument("--disable-dev-shm-usage")  # avoid /dev/shm limits
+    co.set_argument("--remote-debugging-port=0")  # let Chrome pick a free port
     driver = ChromiumPage(co)
     # ---------- end DrissionPage config  ----------
 
